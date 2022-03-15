9e76f1be082bd44726eac0fecb597bc81c5d50e2
Rıza Emre ARAS <emre@frambuaz.dev>
2022-03-16 02:19:31 +0300

    (Update) Changed component theme stylesheet and app-root selector tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
+  selector: 'maas-ist-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
