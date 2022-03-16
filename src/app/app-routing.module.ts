bf8d7247edbd74bfceebd41eff73cd0a391aaecc
Rıza Emre ARAS <emre@frambuaz.dev>
2022-03-16 06:43:00 +0300

    (Create Component) Home component created and routings changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'calculation/form',
+    redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'calculation/form',
